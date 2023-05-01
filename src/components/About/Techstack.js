54f78d24d1db5e49d9f6909a64f2d5de14fc7af5
AAdi23456 <118712556+AAdi23456@users.noreply.github.com>
2023-05-01 14:47:54 +0530

    intial commit

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+// import { CgHTML } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiGit,
+  DiHtml5,
+  DiMysql
+} from "react-icons/di";
+import { FaRedis } from 'react-icons/fa';
+import {
+  SiRedux,
+  SiExpress,
+  SiBootstrap,
+  SiChakraui,
+  faMysql,
+} from "react-icons/si";
+//import RedisIcon from './RedisIcon';
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <SiChakraui />
+      </Col> */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRedux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiExpress />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiBootstrap />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
